47a32da8d6ee54272df04143951fde3f2122e16e
Andrew Hoffman <hoffma@amazon.com>
2020-07-09 14:55:40 -0600

    Bump up max signaling msg length.

--- src/include/com/amazonaws/kinesis/video/webrtcclient/Include.h
+++ src/include/com/amazonaws/kinesis/video/webrtcclient/Include.h
@@ -447,7 +447,7 @@ extern "C" {
 /**
  * Maximum length of signaling message
  */
-#define MAX_SIGNALING_MESSAGE_LEN (10 * 1024)
+#define MAX_SIGNALING_MESSAGE_LEN (20 * 1024)
 /*!@} */
 
 /////////////////////////////////////////////////////
